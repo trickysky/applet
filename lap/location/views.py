a39c7f64ab09296f2aba4861b63907dd30786925
tk <iam@tiankun.me>
2017-02-21 18:23:38 +0800

    initial

---
+++ lap/location/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+
+# Create your views here.
+
+def index(request):
+    return JsonResponse({
+        'code': 0,
+        'msg': 'ok'
+    })
